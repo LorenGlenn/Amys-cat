e02ed85ae0929003e91d2489c42c2d5654a23d02
Amy Hwang and Loren Glenn <loren@>
2016-10-18 11:50:21 -0700

    add a button to toggle between background colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,5 @@
 $(function() {
   $("button#color-change").click(function(){
-    $("body").removeClass();
-    $("body").addClass("dark-background");
-    $(this).attr('id', 'change-back');
-    this.id = 'change-back';
-  });
-  $("button#change-back").click(function(){
-    $("body").removeClass();
-    $("body").addClass("light-background");
-    $(this).attr('id', 'color-change');
-    this.id = 'color-change';
+    $("body").toggleClass("dark-background");
   });
 });
